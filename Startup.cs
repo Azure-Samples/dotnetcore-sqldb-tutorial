60fc81b53f018b2bf405a815bbb006c23de6cf1b
Cephas Lin <cephalin@microsoft.com>
2018-04-11 18:57:45 +0200

    upgrade to core 2.0

--- Startup.cs
+++ Startup.cs
@@ -14,17 +14,12 @@ namespace DotNetCoreSqlDb
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
