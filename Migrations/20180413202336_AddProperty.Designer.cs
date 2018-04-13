6c25ce69f629d94fe44acb1707ec460b5e98fd6f
hvu <hvusyslogicinclocal@outlook.com>
2018-04-13 15:32:00 -0500

    Add Done Property

--- Migrations/20180413202336_AddProperty.Designer.cs
+++ Migrations/20180413202336_AddProperty.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    partial class MyDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180413202336_AddProperty")]
+    partial class AddProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180413202336_AddProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    public partial class AddProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Done",
+                table: "Todo",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Done",
+                table: "Todo");
+        }
+    }
+}
