50844711379d8960912f43777e1a201ff938a4c7
Jonathan Duggan <jonathan.duggan@dynatrace.com>
2019-12-30 14:30:45 -0500

    https://docs.microsoft.com/en-us/azure/app-service/containers/tutorial-dotnetcore-sqldb-app#connect-to-sql-database-in-production

--- Startup.cs
+++ Startup.cs
@@ -36,8 +36,16 @@ namespace DotNetCoreSqlDb
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+            // Use SQL Database if in Azure, otherwise, use SQLite
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<MyDatabaseContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("SQLDB_CONN_STRING")));
+            else
+                services.AddDbContext<MyDatabaseContext>(options =>
+                        options.UseSqlite("Data Source=localdatabase.db"));
+
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
